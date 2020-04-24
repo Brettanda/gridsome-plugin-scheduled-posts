d1304b8994ffd52408e8cc61cc9342253c550649
Brett Anda <brettanda12@gmail.com>
2020-04-24 17:57:32 -0600

    removed the console log for testing

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-
-module.exports = function (api,options) {
+module.exports = function (api, options) {
 	var date = new Date(Date.now());
 	date = new Date(date + date.getTimezoneOffset());
 	date = options.timeZoneOffset.charAt(1) == "-" ? new Date(date.setHours(date.getHours() - parseFloat(options.timeZoneOffset.substr(1)))) : new Date(date.setHours(date.getHours() + parseFloat(options.timeZoneOffset.substr(1))));
 
-	console.log(date, " -- ", options.timeZoneOffset.substr(1));
-	
 	api.onCreateNode(postOptions => {
-		if (postOptions.internal.typeName === options.typeName && Date.parse(date) <= Date.parse(postOptions.date)) return null
+		if (postOptions.internal.typeName === options.typeName && Date.parse(date) <= Date.parse(postOptions.date)) return null;
+
 		return { ...postOptions, id: postOptions.id };
 	});
 };
